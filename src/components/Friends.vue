b9d5f261b67a468b441d30477a639167f7f3bf5d
Gaein nidb <Gaein_nidb_CN@outlook.com>
2021-05-04 20:52:53 +0800

    Update friends Links

--- src/components/Friends.vue
+++ src/components/Friends.vue
@@ -19,7 +19,8 @@
                   <v-card-text>{{ item.description }}</v-card-text>
                   <v-card-actions>
                     <v-btn :href="item.url" color="blue" text>
-                      去看看<v-icon>mdi-arrow-right-circle-outline</v-icon>
+                      去看看
+                      <v-icon>mdi-arrow-right-circle-outline</v-icon>
                     </v-btn>
                   </v-card-actions>
                 </v-card>
@@ -32,11 +33,43 @@
     <v-container>
       <v-card>
         <v-card-title>添加友链</v-card-title>
-        <v-card-subtitle>欢迎来互换友链咯</v-card-subtitle>
+        <v-card-subtitle>欢迎巨佬们来互换友链哦</v-card-subtitle>
         <v-card-text>
+          <v-form>
+            <v-container>
+              <v-card
+                  max-width="800"
+                  class="mx-auto"
+              >
+                <v-card-text>
+                  <v-container>
+                    <v-text-field v-model="user_link.url" prepend-icon="mdi-link" @blur="autoInputTitle" label="网站链接"/>
+                    <v-text-field v-model="user_link.title" prepend-icon="mdi-pen" label="网站标题"/>
+                    <v-text-field v-model="user_link.description" prepend-icon="mdi-card-text" label="网站简介"/>
+                    <v-text-field v-model="user_link.avatar" prepend-icon="mdi-account-edit" label="头像链接"/>
+                    <v-text-field v-model="user_link.mail" prepend-icon="mdi-mail" label="邮箱地址"/>
+                  </v-container>
 
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-container>
+                    <v-btn class="mr-4" color="success">
+                      <v-icon>mdi-send</v-icon>
+                      提交网站信息
+                    </v-btn>
+                    <v-btn color="error" class="mr-4">
+                      <v-icon>mdi-refresh</v-icon>
+                      重置表单
+                    </v-btn>
+                    <v-btn text color="primary">其它请发邮件到 mail@gaein.cn</v-btn>
+                  </v-container>
+                </v-card-actions>
+
+              </v-card>
+            </v-container>
+          </v-form>
         </v-card-text>
-        <v-card-actions></v-card-actions>
       </v-card>
     </v-container>
   </v-container>
@@ -44,6 +77,7 @@
 
 <script>
 import Axios from "axios";
+import HtmlParser from "htmlparser";
 
 export default {
   name: "Friends",
@@ -51,6 +85,13 @@ export default {
     this.getLinks();
   },
   data: () => ({
+    user_link: {
+      avatar: "",
+      title: "",
+      description: "",
+      url: "",
+      mail: ""
+    },
     title: "",
     links: [
       {
@@ -62,6 +103,29 @@ export default {
     ]
   }),
   methods: {
+    autoInputTitle() {
+      console.log("HERE");
+      Axios.get(this.user_link.url)
+          .then(response => {
+            const html = response.data;
+
+            let handler = new HtmlParser.DefaultHandler((error,dom)=>{
+              if(error){
+                console.error(error);
+              } else {
+                console.log(dom);
+              }
+            });
+
+            let parser = new HtmlParser.Parser(handler);
+            parser.parseComplete(html);
+
+            console.log(handler.dom);
+          })
+          .catch(error => {
+            console.error(error);
+          })
+    },
     getLinks() {
       Axios.get("https://static.cdn.gaein.cn/website_used/home_page_data.friends.json")
           .then(response => {
