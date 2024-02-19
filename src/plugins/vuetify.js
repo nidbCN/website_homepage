c26acbd8439f55fdb80a5f9c8d44789a9fb3e244
Gaein nidb <mail@gaein.cn>
2024-02-19 16:02:34 +0800

    feat: add custom fonts

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -6,12 +6,13 @@ Vue.use(Vuetify);
 export default new Vuetify({
     theme: {
         themes: {
-          light: {
-            primary: '#5ccefa',
-            secondary: '#f4a9b7',
-            accent: '#8c9eff',
-            error: '#b71c1c',
-          },
+            light: {
+                fontFamily: 'GlowSansSC, sans-serif',
+                primary: '#5ccefa',
+                secondary: '#f4a9b7',
+                accent: '#8c9eff',
+                error: '#b71c1c',
+            },
         },
-      },
+    },
 });
